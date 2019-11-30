ee7d5cd7b9ac248ffde89ddc3b7494f2ecbd17f6
Jason Ou <ozij@amazon.com>
2019-11-30 11:48:14 -0500

    update UI to use https

--- ui/js/utilities/services.ts
+++ ui/js/utilities/services.ts
@@ -5,47 +5,44 @@ type Services = { [name in AvailableAPIs]: any }
 
 const API_PORT = process.env.API_PORT
 const API_HOST = process.env.API_HOST
-const API_PROTOCOL = 'http'
+const API_PROTOCOL = 'https'
 
 const host = `${API_PROTOCOL}://${API_HOST}:${API_PORT}`
 
-export const services = serviceDefs.reduce(
-  (acc, service) => {
-    const { name, path, ...options } = service
+export const services = serviceDefs.reduce((acc, service) => {
+  const { name, path, ...options } = service
 
-    acc[name] = ({ body }: { body: any } = { body: '{}' }) => {
-      return new Promise(async (resolve, reject) => {
-        try {
-          const url = host + path
+  acc[name] = ({ body }: { body: any } = { body: '{}' }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const url = host + path
 
-          const rawResponse = await fetch(url, {
-            ...options,
-            ...(options.method === 'POST' ? { body } : {}),
-          })
-          const response = await rawResponse.json()
-          const { error, success, status, ...contents } = response
+        const rawResponse = await fetch(url, {
+          ...options,
+          ...(options.method === 'POST' ? { body } : {}),
+        })
+        const response = await rawResponse.json()
+        const { error, success, status, ...contents } = response
 
-          if (success) {
-            resolve({ ...contents, status })
-          } else {
-            reject(
-              error
-                ? { status, error }
-                : {
-                    status: 'Unknown server error',
-                    error: `Unknown server error occured: ${{ ...contents }}`,
-                  }
-            )
-          }
-
-          success ? resolve({ ...contents, status }) : reject({ status, error })
-        } catch (e) {
-          reject({ error: `Unknown error: ${e}`, status: `Error in ${name}` })
+        if (success) {
+          resolve({ ...contents, status })
+        } else {
+          reject(
+            error
+              ? { status, error }
+              : {
+                  status: 'Unknown server error',
+                  error: `Unknown server error occured: ${{ ...contents }}`,
+                }
+          )
         }
-      })
-    }
 
-    return acc
-  },
-  {} as Services
-)
+        success ? resolve({ ...contents, status }) : reject({ status, error })
+      } catch (e) {
+        reject({ error: `Unknown error: ${e}`, status: `Error in ${name}` })
+      }
+    })
+  }
+
+  return acc
+}, {} as Services)

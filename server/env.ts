9ba96fd8450ce1bf578f7226226418e5cd25ef68
Jason Ou <ozij@amazon.com>
2020-01-24 16:27:06 -0800

    hard code

--- server/env.ts
+++ server/env.ts
@@ -3,9 +3,11 @@ import { logger } from './logger'
 export const PORT = process.env.PORT || 443
 export const INSECURE_PORT = process.env.INSECURE_PORT || 80
 export const MODE = process.env.MODE || 'PRODUCTION'
-export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || undefined
+export const AWS_ACCESS_KEY =
+  process.env.AWS_ACCESS_KEY || 'AKIA3JJOCX2YHBMWSPEF'
 export const AWS_SECRET_ACCESS_KEY =
-  process.env.AWS_SECRET_ACCESS_KEY || undefined
+  process.env.AWS_SECRET_ACCESS_KEY ||
+  'fF51/fRH1DYvNdTZHi1+8Z09wzeY+IhdX7T0+Pwv'
 
 export const CORS_URL = process.env.CORS_URL || 'http://localhost:3000'
 export const HOST = process.env.HOST || 'https://mypilea.com'
